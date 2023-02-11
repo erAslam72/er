3829565afea94a08dec9793e3e114fdff21baf2d
erAslam72 <125135802+erAslam72@users.noreply.github.com>
2023-02-11 19:49:23 +0530

    Add files via upload

---
+++ varivale.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int a = 68;
+  int b = 65;
+  int c = 980;
+  int d = 69;
+  float g = 0.5;
+  printf("the value of m is %f \n", g);
+
+  printf("the value a is %d\n", a);
+
+  printf("the value c is %d\n", c);
+  printf("hte value of d is %d\n", d);
+
+  printf("multiply of a ,b is %d\n", a * b);
+  printf("sum of the value a,b,c,d is %d\n", a + b + c);
+  printf("mulyiply of c ,b is %d\n", c, b);
+  printf("sum of the value c, b is %d\n", c + b);
+  printf("the value of d is %d \n", d);
+  printf("the value of m is %f \n", g);
+  float k = 69.5;
+  float j = 69.5;
+  printf("sum of the value of k+ j %f \n ", k + j);
+  printf("multiply k*j %f \n ", j, k);
+  float l = 65.8;
+  float m = 65.9;
+  printf("divided the value of h/l %f\n", l / m);
+  printf("divided the value of h/l %f\n", m / l);
+
+  char f = 'u';
+  char e = 'o';
+  printf("add the value of f+e is  %c \n ", f + e);
+  printf("add the value of f+e is  %c \n ", e + f);
+
+  return 0;
+}
